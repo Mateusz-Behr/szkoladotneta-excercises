9c74f09f14511cf4e6ae650f46c77cbf7bb6c47d
Mateusz Behr <behrmat@gmail.com>
2023-03-25 16:57:57 +0100

    Poprawiono Program 2.4.1

--- Tydzień 2/2.4.1/Program 2.4.1.cs
+++ Tydzień 2/2.4.1/Program 2.4.1.cs
@@ -9,40 +9,72 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Witaj, proszę podaj swoje dane:");
-            Console.WriteLine("Imię:");
 
+
+            Console.WriteLine("Imię:");
             string firstName = Console.ReadLine();
+
 
             Console.WriteLine("Nazwisko:");
             string lastName = Console.ReadLine();
 
+
             Console.WriteLine("Wiek:");
             string enteredAge = Console.ReadLine();
 
-            int age = 0;
-            Int32.TryParse(enteredAge, out age);
+            Int32.TryParse(enteredAge, out int age);
+
 
             Console.WriteLine("Płeć (m/k):");
-
             string enteredGender = Console.ReadLine();
+
             char gender = char.Parse(enteredGender);
 
+
+            Console.WriteLine("Wysokość ciała w cm:");
+            string enteredHeight = Console.ReadLine();
+
+            float height = float.Parse(enteredHeight);
+
+
+            Console.WriteLine("Masa ciała w kg:");
+            string enteredWeight = Console.ReadLine();
+
+            float weight = float.Parse(enteredWeight);
+
+
+            Console.WriteLine("Numer telefonu:");
+            string enteredPhoneNumber = Console.ReadLine();
+
+            int phoneNumber = int.Parse(enteredPhoneNumber);
+
+            Console.WriteLine("Adres email:");
+            string email = Console.ReadLine();
+
+
             Console.WriteLine("PESEL:");
-            string enteredPesel = Console.ReadLine();
+            string pesel = Console.ReadLine();
+
 
             Console.WriteLine("Numer pracownika:");
             string emplNumber = Console.ReadLine();
 
             long employeeNumber = long.Parse(emplNumber);
             Int64.TryParse(emplNumber, out employeeNumber);
-
+            
+            Console.WriteLine("--------------------");
             Console.Write($"Wpisane dane: \r\n" +
                 $"Imię: {firstName}\r\n" +
                 $"Nazwisko: {lastName}\r\n" +
                 $"Wiek: {age}\r\n" +
                 $"Płeć: {gender}\r\n" +
-                $"PESEL: {enteredPesel}\r\n" +
-                $"Nr pracownika: {employeeNumber}");
+                $"Wysokość ciała: {height} cm\r\n" +
+                $"Masa ciała: {weight} kg\r\n" +
+                $"Numer telefonu: {phoneNumber}\r\n" +
+                $"Adres email: {email}\r\n" +
+                $"PESEL: {pesel}\r\n" +
+                $"Nr pracownika: {employeeNumber}\r\n" +
+                "--------------------");
 
         }
     }
