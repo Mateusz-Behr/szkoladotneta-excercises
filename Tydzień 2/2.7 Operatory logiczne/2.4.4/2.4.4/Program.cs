a6a14e6674121afadbc6efaee65819e0cb69529d
Mateusz Behr <behrmat@gmail.com>
2023-04-02 19:54:38 +0200

    Dodano zadania 2.7.1 - 2.7.5

---
+++ Tydzień 2/2.7 Operatory logiczne/2.4.4/2.4.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IsLeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdza czy podany rok jest przestępny.");
+            Console.WriteLine("Podaj rok");
+
+            string a = Console.ReadLine();
+
+            Int32.TryParse(a, out int year);
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) 
+            {
+                Console.WriteLine("Podany rok jest rokiem przestępnym.");
+            }
+            else
+            {
+                Console.WriteLine("Podany rok nie jest rokiem przestępnym.");
+            }
+        }
+    }
+}
---
+++ Tydzień 2/2.7 Operatory logiczne/2.7.3/2.7.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace PositiveNegative
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdza czy liczba jest dodatnia czy ujemna");
+            Console.WriteLine("Podaj liczbę");
+
+            string a = Console.ReadLine();
+            float number = float.Parse(a, CultureInfo.InvariantCulture.NumberFormat);
+
+            if (number < 0)
+            {
+                Console.WriteLine("Podana liczba jest ujemna");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("Podana liczba jest dodatnia");
+            }
+            else 
+            { 
+                Console.WriteLine("Zero nie jest ani dodatnie, ani ujemne."); 
+            }
+        }
+    }
+}
