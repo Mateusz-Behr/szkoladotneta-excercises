ff65bf4ea418bc8c7d7f7c3b20b3a40be145095e
Mateusz Behr <behrmat@gmail.com>
2023-04-02 19:54:38 +0200

    Dodano zadania 2.7.1 - 2.7.5

---
+++ Tydzień 2/2.7 Operatory logiczne/2.7.1/2.7.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace EqualNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            if (a == b)
+            {
+                Console.WriteLine("Liczby są równe.");
+            }
+            else
+            {
+                Console.WriteLine("Liczbą nie są równe.");
+            }
+        }
+    }
+}
---
+++ Tydzień 2/2.7 Operatory logiczne/2.7.2/2.7.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace EvenOrOdd
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdza czy liczba jest parzysta czy nieparzysta.");
+            Console.WriteLine("Podal liczbę:");
+            string a = Console.ReadLine();
+
+            float number = float.Parse(a, CultureInfo.InvariantCulture.NumberFormat);
+            //Int32.TryParse(a, out int number);
+
+            if (number != (int)number)
+            {
+                Console.WriteLine("Podana liczba nie jest całkowita.");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("Podana liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Podana liczba jest nieparzysta.");
+            }
+        } 
+    }
+}
